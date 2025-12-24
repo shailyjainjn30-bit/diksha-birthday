48180b1fadd7693ac99a88f876499e70a46a36cd
shailyjainjn30-bit <shailyjain.jn30@gmail.com>
2025-12-24 21:50:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,27 @@ setInterval(() => {
 
   timer.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }, 1000);
+function startConfetti() {
+  const duration = 5 * 1000;
+  const end = Date.now() + duration;
+
+  (function frame() {
+    confetti({
+      particleCount: 5,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 }
+    });
+    confetti({
+      particleCount: 5,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 }
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
+
